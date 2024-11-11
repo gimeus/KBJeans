ef68d05d7d628a5d3b70e040c0ab2472e1dcc6f2
gimeus <gimeus@naver.com>
2024-11-12 01:38:58 +0900

    feat: Button 컴포넌트 구현

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -1,16 +1,28 @@
+import styled from 'styled-components';
 import HeaderMain from '@/components/HeaderMain';
 import HeaderSub from '@/components/HeaderSub';
+import Button from '@/components/Button';
+import { useNavigate } from 'react-router-dom';
 
 const Onboarding = () => {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate('/');
+  };
+
   return (
     <div>
       <HeaderMain />
       <HeaderSub title="희망 면적 선택" />
-
-      {/* 기타 페이지 내용 */}
-      <div>{/* 여기에 페이지 내용 추가 */}</div>
+      <Footer>
+        <Button onClick={handleButtonClick}>청약 도우미 시작하기</Button>
+      </Footer>
+      <div></div>
     </div>
   );
 };
+
+const Footer = styled.div``;
 
 export default Onboarding;
