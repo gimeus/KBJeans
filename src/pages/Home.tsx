3932fcfae7084fc3d4c608746dfde62d81727c64
gimeus <gimeus@naver.com>
2024-11-11 22:58:44 +0900

    feat: 프로젝트 초기 셋팅

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Home = () => {
+  return <Title>홈</Title>;
+};
+
+const Title = styled.h1`
+  color: var(--g10);
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+export default Home;
