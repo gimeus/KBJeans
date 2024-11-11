4d9b438b4e273e2ca2924e6f72b42b8b4a8dcddc
gimeus <gimeus@naver.com>
2024-11-11 23:58:16 +0900

    feat: HeaderMain 컴포넌트 구현

--- src/pages/Notification.tsx
+++ src/pages/Notification.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import styled from 'styled-components';
 
 const Notification = () => {
-  return <div></div>;
+  return <Title>알림</Title>;
 };
+
+const Title = styled.h1`
+  color: var(--g10);
+  font-size: 24px;
+  font-weight: bold;
+`;
 
 export default Notification;
