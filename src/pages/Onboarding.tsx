18c50e0d1747b48a4a41424792df6290474ab0b0
gimeus <gimeus@naver.com>
2024-11-11 23:50:07 +0900

    feat: HeaderMain 컴포넌트 구현

---
+++ src/pages/Onboarding.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '@/components/HeaderMain';
+
+const Onboarding = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Onboarding;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,10 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home.tsx';
+import KBStarBanking from '../pages/KBStarBanking.tsx';
+import Onboarding from '../pages/Onboarding.tsx';
+import Information from '../pages/Information.tsx';
+import Notification from '../pages/Notification.tsx';
 
 function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<KBStarBanking />} />
+      <Route path="/onboarding" element={<Onboarding />} />
+      <Route path="/information" element={<Information />} />
+      <Route path="/notification" element={<Notification />} />
       {/* 추가 라우트 */}
     </Routes>
   );
