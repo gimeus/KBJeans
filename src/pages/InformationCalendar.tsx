5f9aa28a440e8ad1233369d161e7e25a9f90a4c6
gimeus <gimeus@naver.com>
2024-11-12 15:01:47 +0900

    feat: Tab(1) 컴포넌트 구현

---
+++ src/pages/InformationCalendar.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const InformationCalendar = () => {
+  return <Title>청약일정</Title>;
+};
+
+const Title = styled.h1`
+  color: var(--g10);
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+export default InformationCalendar;
---
+++ src/pages/InformationMap.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const InformationMap = () => {
+  return <Title>청약지도</Title>;
+};
+
+const Title = styled.h1`
+  color: var(--g10);
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+export default InformationMap;
