6ec5e91bbf2228b6ff58a01703a0a357750cfaa6
Jeong Jihee <102722507+dev-9hee@users.noreply.github.com>
2024-11-18 12:09:54 +0900

    fix : 알림 2번 오는 오류

--- src/firebase.ts
+++ src/firebase.ts
@@ -27,8 +27,16 @@ export const requestFirebaseNotificationPermission = async () => {
   try {
     // 웹 푸시 알림을 위한 VAPID 키 설정
     const vapidKey = import.meta.env.VITE_FIREBASE_VAPID_KEY;
-    // FCM 토큰 요청
-    const token = await getToken(messaging, { vapidKey });
+
+    // 명시적으로 등록한 Service Worker를 참조
+    // const registration = await navigator.serviceWorker.ready;
+
+    // FCM 토큰 요청 시 명시적으로 등록한 Service Worker 사용
+    // const token = await getToken(messaging, {
+    //   vapidKey,
+    //   serviceWorkerRegistration: registration,
+    // });
+    const token = await getToken(messaging, vapidKey);
 
     if (token) {
       console.log('Firebase FCM Token:', token);
@@ -59,22 +67,19 @@ export const requestFirebaseNotificationPermission = async () => {
 
 // 포그라운드 상태에서 메시지를 수신했을 때의 핸들러
 onMessage(messaging, (payload) => {
-  console.log('Message received: ', payload);
+  console.log('[React] 포그라운드 메시지 수신: ', payload);
 
   // 알림 데이터의 안전한 접근을 위한 옵셔널 체이닝
   const title = payload.notification?.title;
   const body = payload.notification?.body;
 
+  console.log(title, body);
   // 알림 권한이 있고 필요한 데이터가 있는 경우에만 알림 표시
   if (Notification.permission === 'granted' && title && body) {
     new Notification(title, {
       body,
       icon: '/icons/firebase-logo.png', // 알림에 표시될 아이콘
     });
-  } else {
-    console.warn(
-      'Notification permission not granted or payload is missing required fields.'
-    );
   }
 });
 
